f21b88736486b1cfb630a2d7c21dcd71e922f1e9
Kiley Gross <u0766657@umail.utah.edu>
2020-03-24 18:31:38 -0600

    Added Arduino changes. The CenteringTable shouldn't have changed. It was just from opening it.

---
+++ FinalProject/FinalProject.ino
@@ -0,0 +1,53 @@
+/**********************************************************
+ * Author: Kiley Gross
+ * Date: February 4, 2020
+ *********************************************************/
+#include <DualVNH5019MotorShield.h>
+int leftValIn = A0; // Variable for left motor speed
+int rightValIn = A1; // Variable for right motor speed
+
+signed int leftVal;
+signed int rightVal;
+DualVNH5019MotorShield Motors; // Declare Motors
+
+void setup() {
+  // Begin serial communication
+  Serial.begin(9600);
+  
+  Motors.init(); // Initialize the motors from the library
+
+  // Initialize the pins for reading from PLC
+  pinMode(leftValIn, INPUT);
+  pinMode(rightValIn, INPUT);
+}
+
+void loop() {
+  // Read the values for the left and right motors
+  leftVal = analogRead(leftValIn);
+  rightVal = analogRead(rightValIn);
+
+    //Serial.print("The left value is ");
+    //Serial.println(leftVal);
+
+    //Serial.print("The right value is ");
+    //Serial.println(rightVal);
+  if (leftVal == 0 || rightVal == 0) {
+    Motors.setM1Speed(0);
+    Motors.setM2Speed(0);
+  }
+  else {
+    // Map the values, where 1 corresponds to -400 and 255 corresponds to 400
+    leftVal = map(leftVal, 1, 1023, -400, 400);
+    rightVal = map(rightVal, 1, 1023, -400, 400);
+
+    Serial.print("The left value is ");
+    Serial.println(leftVal);
+
+    Serial.print("The right value is ");
+    Serial.println(rightVal);
+    
+    // Set the motor speeds
+    Motors.setM1Speed(leftVal);
+    Motors.setM2Speed(rightVal);
+  }
+}
